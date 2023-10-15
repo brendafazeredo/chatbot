12216bc37aa631fa2ef75771c262da3b14616bcf
Brenda Azeredo <brendaazeredo@Brendas-MacBook-Air.local>
2023-10-15 13:14:22 -0300

    OPENAI API added.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const sendChatBtn = document.querySelector(".chat-input span");
 const chatbox = document.querySelector(".chatbox");
 
 let userMessage;
+const API_KEY = "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";
 
 const createChatLi = (message, className) => {
     // Create a chat <li> element with passed message and className
@@ -11,7 +12,32 @@ const createChatLi = (message, className) => {
     let chatContent = className === "outgoing" ? `<p>${message}</p>` : `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`;
     chatLi.innerHTML = chatContent;
     return chatLi;
-}
+};
+
+const generateResponse = (incomingChatLi) => {
+    const API_URL = "https://api.openai.com/v1/chat/completions";
+    const messageElement = incomingChatLi.querySelector("p");
+
+    // Define the properties and message for the API request
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: userMessage}]
+        })
+    };
+
+    // Send POST request to API, get response
+    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+        messageElement.textContent = data.choices[0].message.content;
+    }).catch((error) => {
+        messageElement.textContent = "Oops! Something went wrong. Please try again.";
+    })
+};
 
 const handleChat = () => {
     userMessage = chatInput.value.trim();
@@ -22,7 +48,9 @@ const handleChat = () => {
 
     setTimeout(() => {
         // Display a "..." message while waiting for the response
-        chatbox.appendChild(createChatLi("...", "incoming"));
+        const incomingChatLi = createChatLi("...", "incoming");
+        chatbox.appendChild(incomingChatLi);
+        generateResponse(incomingChatLi);
     }, 600);
 };
 
